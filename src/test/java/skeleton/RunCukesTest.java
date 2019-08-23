a0de0fcfeb7c4b5028057a2273fd0ffe2dbcd712
MallikaUppuganti <54269283+MallikaUppuganti@users.noreply.github.com>
2019-08-23 14:39:04 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Testing\\Week 3\\Selenium WorkSpace\\CucumberCaseStudy1\\MyCSFeatures\\MyCSFeatures.feature",glue="stepdefs",tags= {"@UAT"},plugin={"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+@CucumberOptions(plugin={"json:target/cucumber.json"})
 public class RunCukesTest {
 }
